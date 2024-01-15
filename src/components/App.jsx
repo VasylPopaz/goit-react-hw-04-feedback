7340c59ebf868bdffd0686ef4904402db23fe463
VasylPopaz <pafos1997@gmail.com>
2024-01-15 14:18:38 +0100

    update export

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { SectionContainer } from './Section/Section.styled';
 import { Statistics, FeedbackOptions, Section, Notification } from 'components';
 
-export default function App() {
+export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import { GlobalStyle } from 'styles/GlobalStyle';
-import App from 'components/App';
+import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
